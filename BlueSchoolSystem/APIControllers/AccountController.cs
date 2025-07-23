dd8b727aef48250c63b28a307b76d6997a3c9ac5
Nguyen Truong Giang <giangliken@gmail.com>
2025-07-23 16:33:46 +0700

    API cho việc xác nhận đăng nhập

---
+++ BlueSchoolSystem/APIControllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using BlueSchoolSystem.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.Tasks;
+
+namespace BlueSchoolSystem.APIControllers
+{
+    [Route("api/[controller]")]
+
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [EnableRateLimiting("login-policy")]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+                return Unauthorized(new { message = "Email không tồn tại" });
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+
+            if (result.Succeeded)
+            {
+                // Có thể trả token hoặc session nếu cần
+                return Ok(new
+                {
+                    message = "Đăng nhập thành công",
+                    user = new
+                    {
+                        user.Email,
+                        Roles = await _userManager.GetRolesAsync(user)
+                    }
+                });
+            }
+
+            return Unauthorized(new { message = "Mật khẩu không đúng" });
+        }
+    }
+}
