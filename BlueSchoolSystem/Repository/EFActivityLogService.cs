97209b2a9a336a42a0271f28d4b99249469cbf3d
Nguyen Truong Giang <giangliken@gmail.com>
2025-07-29 23:40:20 +0700

    Interface Log và EFLog

---
+++ BlueSchoolSystem/Repository/EFActivityLogService.cs
@@ -0,0 +1,39 @@
+﻿
+using BlueSchoolSystem.Models;
+
+namespace BlueSchoolSystem.Repository
+{
+    public class EFActivityLogService : IActivityLogService
+    {
+        private readonly ApplicationDbContext _context;
+        public EFActivityLogService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<IEnumerable<ActivityLog>> GetAllLogs()
+        {
+            var logs = _context.ActivityLogs
+                .OrderByDescending(log => log.Timestamp)
+                .AsEnumerable();
+            return Task.FromResult(logs);
+        }
+
+        public async Task LogAsync(string userId, string userName, string actionType, string tableName, string objectId, string description)
+        {
+            var log = new ActivityLog
+            {
+                UserId = userId,
+                UserName = userName,
+                ActionType = actionType,
+                TableName = tableName,
+                ObjectId = objectId,
+                Description = description,
+                Timestamp = DateTime.Now
+            };
+
+            _context.ActivityLogs.Add(log);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ BlueSchoolSystem/Repository/IActivityLogService.cs
@@ -0,0 +1,10 @@
+﻿using BlueSchoolSystem.Models;
+
+namespace BlueSchoolSystem
+{
+    public interface IActivityLogService
+    {
+         Task LogAsync(string userId, string userName, string actionType, string tableName, string objectId, string description);
+         Task<IEnumerable<ActivityLog>> GetAllLogs();
+    }
+}
