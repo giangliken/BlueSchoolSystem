d8ec4c56ccb707a2d2a316564a817a3ed8772d4f
Nguyen Truong Giang <giangliken@gmail.com>
2025-08-28 00:05:38 +0700

    Model GV

---
+++ BlueSchoolSystem/Models/ViewModel/CreateGiangVienWithUserRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace BlueSchoolSystem.Models.ViewModel
+{
+    public class CreateGiangVienWithUserRequest
+    {
+        public string UserName { get; set; } // Username để tạo tài khoản
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public GiangVien? GiangVien { get; set; } // Model giảng viên bên bạn đã có
+    }
+}
