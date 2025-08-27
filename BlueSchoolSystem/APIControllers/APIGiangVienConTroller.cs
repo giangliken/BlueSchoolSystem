8c8cb83d8e515b7a2551c77336fd195476741f7d
Nguyen Truong Giang <giangliken@gmail.com>
2025-08-28 00:04:52 +0700

    API cho GV

---
+++ BlueSchoolSystem/APIControllers/APIGiangVienConTroller.cs
@@ -0,0 +1,83 @@
+﻿using BlueSchoolSystem.Models;
+using BlueSchoolSystem.Models.ViewModel;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlueSchoolSystem.APIControllers
+{
+    [Route("api")]
+    [ApiController]
+    public class APIGiangVienConTroller : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public APIGiangVienConTroller(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        //Lấy danh sách giảng viên
+        [Authorize(Roles = "Admin", AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpGet("laydanhsachgiangvien")]
+        public async Task<IActionResult> GetAllGiangVien()
+        {
+            var giangviens = await _context.GiangViens
+                        .Include(gv => gv.Khoa)
+                        
+                        .ToListAsync();
+            var tonggiangvien = await _context.GiangViens.CountAsync();
+            return Ok(new
+            {
+                result = true,
+                code = 200,
+                message = "Lấy dữ liệu thành công",
+                soluonggiangvien = tonggiangvien,
+                data = giangviens
+            });
+        }
+
+        //Thêm giảng viên
+        [Authorize(Roles = "Admin", AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPost("themgiangvien")]
+        public async Task<IActionResult> AddGiangVien([FromBody] CreateGiangVienWithUserRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            // 1. Tạo user
+            var user = new ApplicationUser
+            {
+                UserName = request.UserName,
+                Email = request.Email,
+                PhoneNumber = request.PhoneNumber,
+            };
+            var result = await _userManager.CreateAsync(user, request.Password);
+            if (!result.Succeeded)
+                return BadRequest(result.Errors);
+
+            await _userManager.AddToRoleAsync(user, SD.Role_Teacher);
+
+            // 2. Gán UserId vào giảng viên
+            var giangvien = request.GiangVien;
+            giangvien.UserId = user.Id;
+            giangvien.CreatedAt = DateTime.Now;
+            giangvien.UpdatedAt = DateTime.Now;
+
+            // 3. Lưu vào DB
+            _context.GiangViens.Add(giangvien);
+            await _context.SaveChangesAsync();
+
+            return Ok(new
+            {
+                message = "Tạo giảng viên và tài khoản thành công!",
+                studentId = giangvien.Id,
+                userId = user.Id
+            });
+        }
+
+    }
+}
