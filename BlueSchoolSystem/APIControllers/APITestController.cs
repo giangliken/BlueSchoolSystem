82dc211a94cb842e1667986777bf25f68456481c
Nguyen Truong Giang <giangliken@gmail.com>
2025-08-29 20:05:31 +0700

    APITestController

--- BlueSchoolSystem/APIControllers/APITestController.cs
+++ BlueSchoolSystem/APIControllers/APITestController.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BlueSchoolSystem.APIControllers
 {
-    [Route("api/[controller]")]
+    [Route("api")]
     [ApiController]
     public class APITestController : ControllerBase
     {
@@ -37,61 +37,7 @@ namespace BlueSchoolSystem.APIControllers
             });
         }
 
-        [HttpPost("add")]
-        public async Task<IActionResult> AddStudent([FromBody] SinhVien student)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            // Auto set ngày tạo
-            student.CreatedAt = DateTime.Now;
-            student.UpdatedAt = DateTime.Now;
-
-            _context.SinhViens.Add(student);
-            await _context.SaveChangesAsync();
-
-            return Ok(new { message = "Student added successfully!" });
-        }
-
-        
-
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        [HttpGet("me")]
-        public async Task<IActionResult> GetCurrentUser()
-        {
-            var userId = User.FindFirst("userId")?.Value;
-
-            if (userId == null)
-                return Unauthorized();
-
-            var user = await _userManager.Users
-                .Include(u => u.SinhViens)
-                .FirstOrDefaultAsync(u => u.Id == userId);
-
-            if (user == null)
-                return NotFound(new { message = "User không tồn tại!" });
-
-            return Ok(new
-            {
-                result = true,
-                code = 200,
-                user.Id,
-                user.UserName,
-                user.Email,
-                //user.Student.MSSV,
-                //ho_dem = $"{user.Student.HoVaTenDem}",
-                //ten = $"{user.Student.Ten}",
-                //user.Student.MaLop,
-                //user.Student.MaKhoa,
-                //user.Student.MaNganh,
-                //user.Student.TrangThai
-            });
-        }
-
-
-        [HttpGet]
+        [HttpGet("healthcheck")]
         public IActionResult Ping()
         {
             return Ok(new { message = "Server is alive" });
