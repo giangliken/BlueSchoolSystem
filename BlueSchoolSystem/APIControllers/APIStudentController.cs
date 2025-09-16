eea118f230f29820d62c59e089366369f2397e28
Nguyen Truong Giang <giangliken@gmail.com>
2025-09-16 23:39:32 +0700

    Cập nhật API

--- BlueSchoolSystem/APIControllers/APIStudentController.cs
+++ BlueSchoolSystem/APIControllers/APIStudentController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace BlueSchoolSystem.APIControllers
 {
