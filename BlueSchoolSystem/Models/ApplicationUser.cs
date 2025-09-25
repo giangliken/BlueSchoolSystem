df692109c9912333d6769259c6bff87d537fc07d
Nguyen Truong Giang <giangliken@gmail.com>
2025-09-26 00:11:54 +0700

    Cập nhật View Model

--- BlueSchoolSystem/Models/ApplicationUser.cs
+++ BlueSchoolSystem/Models/ApplicationUser.cs
@@ -8,5 +8,12 @@ namespace BlueSchoolSystem.Models
 
         public virtual SinhVien? SinhViens { get; set; }
         public virtual GiangVien? GiangViens { get; set; }
+        [Required(ErrorMessage =("Số điện thoại là bắt buộc"))]
+        [RegularExpression(@"^0\d{9}$", ErrorMessage = "Số điện thoại phải có 10 số và bắt đầu bằng 0.")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage =("Email là bắt buộc"))]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ.")]
+        public override string Email { get; set; }
     }
 }
