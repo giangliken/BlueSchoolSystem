cb70662019387d44263b853fa4e523988c1f38a5
tai vo <votai2804@gmail.com>
2025-09-30 18:10:14 +0700

    viewmodel theo lophocphan theo hoc ki

---
+++ BlueSchoolSystem/Models/ViewModel/LopHocPhanHocKyViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BlueSchoolSystem.Models.ViewModel
+{
+    public class LopHocPhanHocKyViewModel
+    {
+        public int HocKyId { get; set; }
+        public string TenHocKy { get; set; }
+        public DateTime NgayBatDau { get; set; }
+        public List<LopHocPhanMonHocViewModel> DanhSachMon { get; set; } = new List<LopHocPhanMonHocViewModel>();
+    }
+}
---
+++ BlueSchoolSystem/Models/ViewModel/LopHocPhanMonHocViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BlueSchoolSystem.Models.ViewModel
+{
+    public class LopHocPhanMonHocViewModel
+    {
+        public string MaMonHoc { get; set; }
+        public string TenMonHoc { get; set; }
+        public int SoTinChi { get; set; }
+        public string MaLopHocPhan { get; set; }
+    }
+}
