a16accb4ddabe3683b63301081798eab31accf31
tai vo <votai2804@gmail.com>
2025-10-02 21:43:40 +0700

    them viewmodel cho api diem danh

---
+++ BlueSchoolSystem/Models/ViewModel/DiemDanhResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace BlueSchoolSystem.Models.ViewModel
+{
+    public class DiemDanhResponse<T>
+    {
+        public bool Result { get; set; }
+        public int Code { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public int SoLuong { get; set; }
+        public T Data { get; set; } = default!;
+    }
+}
---
+++ BlueSchoolSystem/Models/ViewModel/DiemDanhViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlueSchoolSystem.Models.ViewModel
+{
+    public class DiemDanhViewModel
+    {
+        public DateTime Ngay { get; set; }
+        public string? TenTrangThai { get; set; }
+        public string? GhiChu { get; set; }
+    }
+}
