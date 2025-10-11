4575f6baead68e73c186bd321dd25f119f09e2f0
Nguyen Truong Giang <giangliken@gmail.com>
2025-10-11 22:38:22 +0700

    Cập nhật Model

--- BlueSchoolSystem/Models/ChiTietDiemDanh.cs
+++ BlueSchoolSystem/Models/ChiTietDiemDanh.cs
@@ -8,8 +8,13 @@
         public int SinhVienId { get; set; } // ID của sinh viên
         public SinhVien? SinhVien { get; set; } // Tham chiếu đến sinh viên
         public DateTime ThoiGian { get; set; } // Thời gian điểm danh
+
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
         public int TrangThaiId { get; set; } // Trạng thái điểm danh (ví dụ: 0 - Vắng, 1 - Có mặt, 2 - Muộn)
         public TrangThai? TrangThai { get; set; } // Liên kết với bảng trạng thái
+        public string? DeviceId { get; set; }
+
         public string? GhiChu { get; set; } // Ghi chú thêm về chi tiết điểm danh
 
     }
--- BlueSchoolSystem/Models/DiemDanh.cs
+++ BlueSchoolSystem/Models/DiemDanh.cs
@@ -5,6 +5,10 @@
         public int Id { get; set; }
         public int LopHocPhanId { get; set; } // ID của lớp học phần
         public LopHocPhan? LopHocPhan { get; set; } // Tham chiếu đến lớp học phần
+        public string Code { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ExpireAt { get; set; }
+
         public DateTime Ngay { get; set; } // Ngày điểm danh
         public int TrangThaiId { get; set; } // Trạng thái điểm danh (ví dụ: 0 - Vắng, 1 - Có mặt, 2 - Muộn)
         public TrangThai? TrangThai { get; set; } // Liên kết với bảng trạng thái
