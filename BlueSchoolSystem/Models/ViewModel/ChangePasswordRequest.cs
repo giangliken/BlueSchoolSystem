8d7e6e2bfb5c80301699013a38d0486a63dc066d
Nguyen Truong Giang <giangliken@gmail.com>
2025-10-24 17:41:57 +0700

    Thêm ViewModel đổi mật khẩu

---
+++ BlueSchoolSystem/Models/ViewModel/ChangePasswordRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BlueSchoolSystem.Models.ViewModel
+{
+    public class ChangePasswordRequest
+    {
+        public string CurrentPassword { get; set; } = default!;
+        public string NewPassword { get; set; } = default!;
+    }
+}
