60f7e554b833d564746789c8367fb3b6ef6b3b2d
Nguyen Truong Giang <giangliken@gmail.com>
2025-11-26 23:59:00 +0700

    View Model Auto Add Class

---
+++ BlueSchoolSystem/Models/ViewModel/AutoClassRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace BlueSchoolSystem.Models.ViewModel
+{
+    public class AutoClassRequest
+    {
+        public int Khoa { get; set; }     // 2022
+        public int NganhId { get; set; }  // ngành đã chọn
+        public int SoLuong { get; set; }  // số lớp muốn tạo
+    }
+}
