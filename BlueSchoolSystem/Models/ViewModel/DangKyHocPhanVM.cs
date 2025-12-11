2163e5c89c0dd34397a37a87c919b0960cf8f881
tai vo <votai2804@gmail.com>
2025-12-11 23:10:08 +0700

    nay push thieu

---
+++ BlueSchoolSystem/Models/ViewModel/DangKyHocPhanVM.cs
@@ -0,0 +1,16 @@
+﻿namespace BlueSchoolSystem.Models.ViewModel
+{
+    public class DangKyHocPhanVM
+    {
+        public int Id { get; set; } // ID Lớp học phần
+        public string MaLopHocPhan { get; set; }
+        public string TenMonHoc { get; set; }
+        public int SoTinChi { get; set; }
+        public string GiangVien { get; set; }
+        public string LichHoc { get; set; } // Chuỗi hiển thị lịch (VD: "Thứ 2 (Tiết 1-3)")
+        public int SiSo { get; set; }
+        public int DaDangKy { get; set; } // Số lượng đã đăng ký hiện tại
+        public bool IsRegistered { get; set; } // Sinh viên hiện tại đã đăng ký lớp này chưa?
+        public string TrangThaiLop { get; set; }
+    }
+}
---
+++ BlueSchoolSystem/Models/ViewModel/HocPhiViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlueSchoolSystem.Models.ViewModel
+{
+    public class HocPhiViewModel
+    {
+        public decimal DuNoHienTai { get; set; }
+        public List<ChiTietHocPhi> LichSuPhatSinh { get; set; }
+        public List<PhieuThu> LichSuDongTien { get; set; }
+    }
+}
